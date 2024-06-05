1803190a3dffd44fdfddc4cbbdbb7dc3fa6a597e
SeoungYeop-Baek <tmdduq1999@naver.com>
2024-06-05 13:50:05 +0900

    config

--- src/main/java/Collabo/MoITZY/web/config/WebConfig.java
+++ src/main/java/Collabo/MoITZY/web/config/WebConfig.java
@@ -1,4 +1,4 @@
-package Collabo.MoITZY.webconfig;
+package Collabo.MoITZY.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
