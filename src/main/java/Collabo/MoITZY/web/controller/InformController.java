8e83a64ce88041b07012cdd5b13b79705273ad7c
tlswl7479 <tlswl7479@gmail.com>
2024-06-05 13:02:16 +0900

    .

--- src/main/java/Collabo/MoITZY/web/controller/InformController.java
+++ src/main/java/Collabo/MoITZY/web/controller/InformController.java
@@ -3,18 +3,21 @@ package Collabo.MoITZY.web.controller;
 import Collabo.MoITZY.domain.Inform;
 import Collabo.MoITZY.web.service.InformService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class InformController {
 
     private final InformService informService;
 
-    @GetMapping("/main")
+    @GetMapping("/mo-itzy/main")
     public List<Inform> getInforms() {
         return informService.getAllInforms();
     }
